bbb4bde0339dd4eeedb0b194e9152dabe69d9271
Skuad <skuad@Skuads-MacBook-Air.local>
2025-08-19 21:07:31 +0530

    Made some changes

--- Driver.java
+++ Driver.java
@@ -4,27 +4,21 @@ public class Driver {
     public static void main(String[] args) {
         List<Book> books = DatasetReader.readBooksFromCSV("data.csv");
 
-        // 1. Total number of books by an author
         System.out.println("Total books by George Orwell: " + countBooksByAuthor(books, "George Orwell"));
 
-        // 2. All authors
         System.out.println("\nAll authors:");
         printAllAuthors(books);
 
-        // 3. All books by an author
         System.out.println("\nBooks by George R. R. Martin:");
         printBooksByAuthor(books, "George R. R. Martin");
 
-        // 4. Classify by user rating
         System.out.println("\nBooks with 4.7 rating:");
         printBooksByRating(books, 4.7);
 
-        // 5. Price of all books by an author
         System.out.println("\nBooks and prices by JJ Smith:");
         printBooksAndPricesByAuthor(books, "JJ Smith");
     }
 
-    // Task 1
     public static int countBooksByAuthor(List<Book> books, String author) {
         int count = 0;
         for (Book b : books) {
@@ -35,7 +29,6 @@ public class Driver {
         return count;
     }
 
-    // Task 2
     public static void printAllAuthors(List<Book> books) {
         Set<String> authors = new HashSet<>();
         for (Book b : books) {
@@ -46,7 +39,6 @@ public class Driver {
         }
     }
 
-    // Task 3
     public static void printBooksByAuthor(List<Book> books, String author) {
         for (Book b : books) {
             if (b.getAuthor().equalsIgnoreCase(author)) {
@@ -55,7 +47,6 @@ public class Driver {
         }
     }
 
-    // Task 4
     public static void printBooksByRating(List<Book> books, double rating) {
         for (Book b : books) {
             if (b.getUserRating() == rating) {
@@ -64,7 +55,6 @@ public class Driver {
         }
     }
 
-    // Task 5
     public static void printBooksAndPricesByAuthor(List<Book> books, String author) {
         for (Book b : books) {
             if (b.getAuthor().equalsIgnoreCase(author)) {
